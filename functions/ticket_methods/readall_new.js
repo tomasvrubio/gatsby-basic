4a9ad8e7d79152608a3c266ab171aee90fb73343
tomasvrubio <tomasvrubio@gmail.com>
2019-12-26 07:30:02 +0100

    Creando API con mongoose basandome en una de FaunaDB

---
+++ functions/ticket_methods/readall_new.js
@@ -0,0 +1,9 @@
+// readall.js
+import mongoose from 'mongoose'
+
+// Connect to database
+import db from './server'
+
+// Load the model
+import Ticket from './ticketModel'
+
---
+++ functions/ticket_methods/server.js
@@ -0,0 +1,30 @@
+// server.js
+import mongoose from 'mongoose'
+
+// Estas líneas tienen que ir en la parte superior de todas los ficheros de función para poder cargar variables de entorno de desarrollo
+// --------------------------------------------------------
+const envConfig = require("dotenv").config({
+  path: `.env.development`,
+});
+//Para cargar una variable en concreto: process.env.NOMBRE_V = envConfig.parsed.NOMBRE_V
+//Para cargar todas las variables en el fichero de entorno:
+Object.entries(envConfig.parsed || {}).forEach(
+  ([key, value]) => (process.env[key] = value)
+);
+// ---------------------------------------------------------
+
+
+// Initialize connection to database
+const dbUrl = process.env.GATSBY_MONGODB_URI,
+      dbOptions = {
+        useNewUrlParser: true,
+        useFindAndModify: false
+      }
+
+// Set DB from mongoose connection
+mongoose.connect(dbUrl, dbOptions)
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+export default db

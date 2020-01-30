4f7f834390023c2b700be2af7a69241abcad049d
Tomás <tomasvrubio@gmail.com>
2020-01-30 07:57:13 +0000

    NEW route functions

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,9 @@ import { Link } from "gatsby"
 export default () => (
   <div>
     <p>Hello Gatsby!</p>
-    <p>{process.env.NUEVO}</p>
+    <p>{process.env.GATSBY_FUNC_PATH}</p>
     <Link to="/account">Go to your account</Link>
     {/* <a href="/.netlify/functions/hello">Trigger Function here</a> */}
-    <a href={process.env.GATSBY_PATH_FUNCTIONS}>Trigger Function here</a>
+    <a href={process.env.GATSBY_FUNC_PATH + "/hello"}>Trigger hello Function here</a>
   </div>
 )

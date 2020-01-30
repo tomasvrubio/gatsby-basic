1e3f32f9abee3aa3b14247b51931def98811b8b2
tomasvrubio <tomasvrubio@gmail.com>
2020-01-30 07:32:47 +0100

    NEW trigger functions in each deployment

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,9 @@ import { Link } from "gatsby"
 export default () => (
   <div>
     <p>Hello Gatsby!</p>
+    <p>{process.env.NUEVO}</p>
     <Link to="/account">Go to your account</Link>
-    <a href="/.netlify/functions/hello">Trigger Function here</a>
+    {/* <a href="/.netlify/functions/hello">Trigger Function here</a> */}
+    <a href={process.env.GATSBY_PATH_FUNCTIONS}>Trigger Function here</a>
   </div>
 )

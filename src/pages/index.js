ecf9541647cd5dc38c38a8431ad5a132b5096fec
tomasvrubio <tomasvrubio@gmail.com>
2020-02-12 07:29:33 +0100

    NEW keep adding grommet to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {PureComponent, useState} from "react"
 import { Link } from "gatsby"
 import { Box, Button, Heading, Grommet } from "grommet"
 import { Notification } from "grommet-icons"
@@ -30,19 +30,97 @@ const AppBar = (props) => (
   />
 );
 
-export default () => (
-  <Grommet theme={ theme }>
-    <AppBar>
-      Hello Grommet!
-      <Heading level='3' margin='none'>My App</Heading>
-      <Button icon={<Notification />} onClick={() => {}} />
-    </AppBar>
-    <div>
-      <p>Hello Gatsby!</p>
-      <p>{process.env.GATSBY_FUNC_PATH}</p>
-      <Link to="/account">Go to your account</Link>
-      {/* <a href="/.netlify/functions/hello">Trigger Function here</a> */}
-      <a href={process.env.GATSBY_FUNC_PATH + "/hello"}>Trigger hello Function here</a>
-    </div>
-  </Grommet>
-)
+// const [showSidebar, setShowSidebar] = useState(false)
+
+// export default () => (
+//   <Grommet theme={theme} full>
+//     <Box fill>
+//       <AppBar>
+//         Hello Grommet!
+//         <Heading level='3' margin='none'>My App</Heading>
+//         <Button
+//           icon={<Notification />}
+//           onClick={() => setShowSidebar(!showSidebar)}
+//         />
+//       </AppBar>
+//       <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
+//       <Box flex align='center' justify='center'>
+//         app body
+//         <div>
+//           <p>Hello Gatsby!</p>
+//           <p>{process.env.GATSBY_FUNC_PATH}</p>
+//           <Link to="/account">Go to your account</Link>
+//           {/* <a href="/.netlify/functions/hello">Trigger Function here</a> */}
+//           <a href={process.env.GATSBY_FUNC_PATH + "/hello"}>Trigger hello Function here</a>
+//         </div>
+//       </Box>
+//       {showSidebar && (
+//         <Box
+//           width='medium'
+//           background='light-2'
+//           elevation='small'
+//           align='center'
+//           justify='center'
+//         >
+//           sidebar
+//         </Box>
+//       )}
+//       </Box>
+//     </Box>
+
+//   </Grommet>
+// )
+
+
+// class IndexPage extends PureComponent {
+
+//   render() {
+    
+const IndexPage = () => {    
+    const [showSidebar, setShowSidebar] = useState(false)
+
+    return (
+      <Grommet theme={theme} full>
+        <Box fill>
+          <AppBar>
+            Hello Grommet!
+            <Heading level='3' margin='none'>My App</Heading>
+            <Button
+              icon={<Notification />}
+              onClick={() => setShowSidebar(!showSidebar)}
+            />
+          </AppBar>
+          <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
+          <Box flex align='center' justify='center'>
+            app body
+            <div>
+              <p>Hello Gatsby!</p>
+              <p>{process.env.GATSBY_FUNC_PATH}</p>
+              <Link to="/account">Go to your account</Link>
+              {/* <a href="/.netlify/functions/hello">Trigger Function here</a> */}
+              <a href={process.env.GATSBY_FUNC_PATH + "/hello"}>Trigger hello Function here</a>
+            </div>
+          </Box>
+          {showSidebar && (
+            <Box
+              width='medium'
+              background='light-2'
+              elevation='small'
+              align='center'
+              justify='center'
+            >
+              sidebar
+            </Box>
+          )}
+          </Box>
+        </Box>
+
+      </Grommet>
+    )
+  // }
+}
+
+
+
+
+export default IndexPage
